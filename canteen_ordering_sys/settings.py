e7df8b51a93d53eb280ad53e93aed4fae5867d04
Shaleen Malik <84655260+Shaleen-62@users.noreply.github.com>
2024-02-15 12:36:43 +0530

    Update settings.py csrf

--- canteen_ordering_sys/settings.py
+++ canteen_ordering_sys/settings.py
@@ -103,6 +103,8 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
+CSRF_TRUSTED_ORIGINS = ["https://delivery-service-iit-bhilai.onrender.com"]
+
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
